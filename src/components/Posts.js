9b4983b668eaff36d59b1d36d88915511e95c727
SpLiYvIiI <atukadagio123@gmail.com>
2020-12-28 19:29:35 +0400

    change
    clear input

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,13 +2,15 @@ import React from 'react'
 import {useSelector} from 'react-redux'
 import {Table} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
-
+import {useDispatch} from 'react-redux'
+import {setSearchBy} from '../reducer/filterRerducer'
 
 const Posts  = () => {
     const posts = useSelector(state => {
         if(state.filter === '') return state.posts
         else return state.posts.filter(post => post.title.toLowerCase().indexOf(state.filter.toLowerCase()) !== -1)
     })
+    const dispatch = useDispatch()
     return(
     <div>
         <Table striped bordered hover variant="dark">
@@ -26,7 +28,7 @@ const Posts  = () => {
                 <tr key={post.id}>
                   <td>{post.id}</td>
                   <td>{post.title}</td>
-                  <td><Link to={`/details/${post.id}`}>details</Link></td>
+                  <td><Link onClick={()=>{dispatch(setSearchBy(''))}} to={`/details/${post.id}`}>details</Link></td>
                 </tr>
               )
             })
